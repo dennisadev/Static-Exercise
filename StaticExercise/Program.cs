dbae7a997166ec5160e0d297e880e68b1628b54e
Dennis <dennisadev@gmail.com>
2024-12-16 11:31:24 -0500

    Inital Commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace StaticExercise
+﻿using System;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Fahrenheit To Celsius");
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(32)); 
+
+            Console.WriteLine("Celsius To Fahrenheit");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempInF) 
+        {
+            return (tempInF - 32) * (9/5);
+        }
+
+        public static double CelsiusToFahrenheit(double tempInC) 
+        {
+            return (tempInC * 9 / 5) + 32;
+        }
+    }
+}
